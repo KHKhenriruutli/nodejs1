7488167568fabd06103ca27723b0fd3dfd5f2d16
KHKHenriRuutli <henri.ruutli@khk.ee>
2022-01-26 09:25:31 +0200

    split the query string

--- myprogram.js
+++ myprogram.js
@@ -1,11 +1,17 @@
 var http = require('http');
+var url = require('url');
 var dt = require('./myprogram.js');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end('Hello World!');
-}).listen(8080);
 
 exports.myDateTime = function () {
   return Date();
 };
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end(txt);
+}).listen(8080);
+
