fa2fa436c590bf057dcfccb9e1d0b90916c381ee
KHKHenriRuutli <henri.ruutli@khk.ee>
2022-01-27 13:04:52 +0200

    URL module

--- myprogram.js
+++ myprogram.js
@@ -7,6 +7,22 @@ exports.myDateTime = function () {
   return Date();
 };
 
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
+/*
+
 
 http.createServer(function (req, res) {
   fs.appendFile("mynewfile1.txt", "Hello Content!", function(err) {
@@ -24,3 +40,4 @@ http.createServer(function (req, res) {
   return res.end(txt);
 }).listen(8080);
 
+*/
